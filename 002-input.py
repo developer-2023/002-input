e8c3d22612305eb2a7f97e2888f1d6ebfc001abf
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-05 20:47:22 +0100

    Initial commit

---
+++ 002-input.py
@@ -0,0 +1,14 @@
+# PSEUDOCODE
+# Ask user for his name
+# Say hello to user
+
+# Ask user for his name
+print("What's your name? ")
+input()
+# Say hello to user
+print("hello my friend")
+
+# Ask user for his name but do it in one line
+input("What's your name? ")
+# Say hello to user
+print("hello my friend")
